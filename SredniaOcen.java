8fc03fb29febe34201ce7ac7b23b51abb4818ace
Adam Ostoja-Helczynski <ahelczynski@gmail.com>
2024-04-06 12:12:08 +0200

    zad1

---
+++ SredniaOcen.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SredniaOcen {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj liczbę ocen: ");
+        int iloscOcen = scanner.nextInt();
+
+        double[] ocenyUczniow = new double[iloscOcen];
+        double sumaOcen = 0.0;
+
+        for (int i = 0; i < iloscOcen; i++) {
+            System.out.print("Podaj ocenę " + (i + 1) + ": ");
+            ocenyUczniow[i] = scanner.nextDouble();
+            sumaOcen += ocenyUczniow[i];
+        }
+
+        double sredniaOcen = sumaOcen / iloscOcen;
+        System.out.println("Średnia ocen: " + sredniaOcen);
+    }
+}
